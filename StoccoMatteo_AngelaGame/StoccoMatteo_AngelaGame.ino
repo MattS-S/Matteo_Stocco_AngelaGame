2662826f8252a41640fefb0ef17f2add666cec8a
Matteo <matteo.stocco@barsanti.edu.it>
2019-01-21 18:42:46 +0100

    21/01/19 Avanzamento scelta giocatore

--- StoccoMatteo_AngelaGame/StoccoMatteo_AngelaGame.ino
+++ StoccoMatteo_AngelaGame/StoccoMatteo_AngelaGame.ino
@@ -7,6 +7,7 @@ int turnopartita = 0;
 int numerogiocatore1 = 0;
 int numerogiocatore2 = 0;
 int punteggioobiettivo = 30;
+int punteggiomomentaneo = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -19,15 +20,19 @@ void setup() {
 void inseriscipunteggioobiettivo()
 {
   punteggioobiettivo = Serial.parseInt();
-  while (punteggioobiettivo == 0)
+  while (punteggioobiettivo < 30)
   {
-    punteggioobiettivo = Serial.parseInt();
+    if (punteggioobiettivo == 0)
+    {
+      punteggioobiettivo = Serial.parseInt();
+    } else {
+      punteggioobiettivo = Serial.parseInt();
+      Serial.println("Punteggio obiettivo non valido");
+    }
   }
   if ((punteggioobiettivo >= 30) && (punteggioobiettivo < 100))
   {
     Serial.println(punteggioobiettivo);
-  } else {
-    Serial.println("Punteggio obiettivo non valido");
   }
 }
 
@@ -36,15 +41,82 @@ void sceltagiocatore()
   if (turnopartita == 0)
   {
     numerogiocatore1 = Serial.parseInt();
-    while (numerogiocatore1 == 0)
+    while ((numerogiocatore1 <= 0) || (numerogiocatore1 > 6))
     {
-      numerogiocatore1 = Serial.parseInt();
+      if (numerogiocatore1 == 0)
+      {
+        numerogiocatore1 = Serial.parseInt();
+      } else {
+        numerogiocatore1 = Serial.parseInt();
+        Serial.println("Numero scelto non valido");
+      }
     }
     if ((numerogiocatore1 >= 0) && (numerogiocatore1 < 7))
     {
       Serial.println(numerogiocatore1);
-    } else {
-      Serial.println("Numero scelto non valido");
+      punteggiomomentaneo = punteggiomomentaneo + numerogiocatore1;
+      Serial.print("Punteggio momentaneo: ");
+      Serial.println(punteggiomomentaneo);
+      turnogiocatore = 1;
+      turnopartita++;
+    }
+  } else {
+    if (turnogiocatore == 1)
+    {
+      numerogiocatore2 = Serial.parseInt();
+      switch (numerogiocatore1)
+      {
+        case 1:
+        case 6:
+          while ((numerogiocatore2  == 1) || (numerogiocatore2  == 6) || (numerogiocatore2 > 6) || (numerogiocatore2 <= 0))
+          {
+            numerogiocatore2 = Serial.parseInt();
+            if ((numerogiocatore2  == 1) || (numerogiocatore2  == 6) || (numerogiocatore2 > 6))
+            {
+              Serial.println("Numero scelto non valido");
+            }
+          }
+          Serial.println(numerogiocatore2);
+          punteggiomomentaneo = punteggiomomentaneo + numerogiocatore2;
+          Serial.print("Punteggio momentaneo: ");
+          Serial.println(punteggiomomentaneo);
+          turnogiocatore = 0;
+          turnopartita++;
+          
+        case 2:
+        case 5:
+          while ((numerogiocatore2  == 2) || (numerogiocatore2  == 5) || (numerogiocatore2 > 6) || (numerogiocatore2 <= 0))
+          {
+            numerogiocatore2 = Serial.parseInt();
+            if ((numerogiocatore2  == 2) || (numerogiocatore2  == 5) || (numerogiocatore2 > 6))
+            {
+              Serial.println("Numero scelto non valido");
+            }
+          }
+          Serial.println(numerogiocatore2);
+          punteggiomomentaneo = punteggiomomentaneo + numerogiocatore2;
+          Serial.print("Punteggio momentaneo: ");
+          Serial.println(punteggiomomentaneo);
+          turnogiocatore = 0;
+          turnopartita++;
+
+        case 3:
+        case 4:
+          while ((numerogiocatore2  == 3) || (numerogiocatore2  == 4) || (numerogiocatore2 > 6) || (numerogiocatore2 <= 0))
+          {
+            numerogiocatore2 = Serial.parseInt();
+            if ((numerogiocatore2  == 3) || (numerogiocatore2  == 4) || (numerogiocatore2 > 6))
+            {
+              Serial.println("Numero scelto non valido");
+            }
+          }
+          Serial.println(numerogiocatore2);
+          punteggiomomentaneo = punteggiomomentaneo + numerogiocatore2;
+          Serial.print("Punteggio momentaneo: ");
+          Serial.println(punteggiomomentaneo);
+          turnogiocatore = 0;
+          turnopartita++;
+      }
     }
   }
 }
@@ -52,7 +124,10 @@ void sceltagiocatore()
 
 void loop() {
   // put your main code here, to run repeatedly:
-  inseriscipunteggioobiettivo();
+  if (turnopartita == 0)
+  {
+    inseriscipunteggioobiettivo();
+  }
   sceltagiocatore();
 
 }
