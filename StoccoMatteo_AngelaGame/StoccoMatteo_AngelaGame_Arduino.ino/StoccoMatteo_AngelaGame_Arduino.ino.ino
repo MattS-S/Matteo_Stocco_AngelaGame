c3974018b25ff46ce5df2bd95e3b424c0778d114
Matteo <matteo.stocco@barsanti.edu.it>
2019-02-08 09:50:37 +0100

    8/02/2019 Metodo scegliere punteggio lcd

--- StoccoMatteo_AngelaGame/StoccoMatteo_AngelaGame_Arduino.ino/StoccoMatteo_AngelaGame_Arduino.ino.ino
+++ StoccoMatteo_AngelaGame/StoccoMatteo_AngelaGame_Arduino.ino/StoccoMatteo_AngelaGame_Arduino.ino.ino
@@ -2,11 +2,12 @@
 
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
+int numeroselezionato = 0;
 int turnogiocatore = 0;
 int turnopartita = 0;
-int numerogiocatore1 = 0;
-int numerogiocatore2 = 0;
-int punteggioobiettivo = 0;
+int ngio1 = 0;
+int ngio2 = 0;
+int punteggioobiettivo = 30;
 int punteggiomomentaneo = 0;
 
 int btnpremuto = 0;
@@ -34,9 +35,79 @@ void setup() {
   lcd.begin(16, 2);
   lcd.setCursor(2, 0);
   lcd.print("Angela Game");
+  delay(3000);
 }
 
+
+void aspetta()
+{
+  btnpremuto = BottonePremuto();
+  while (btnpremuto != SELECT)
+  {
+    btnpremuto = BottonePremuto();
+  }
+}
+
+void cambiaselezione()
+{
+  if (BottonePremuto() == RIGHT)
+  {
+    if (numeroselezionato == 0)
+    {
+      numeroselezionato = 1;
+    } else {
+      numeroselezionato = 0;
+    }
+  }
+}
+
+void controlloselezione()
+{
+  if (numeroselezionato == 0)
+  {
+    punteggioobiettivo += 10;
+    lcd.setCursor(7, 1);
+    lcd.print(punteggioobiettivo);
+  } else {
+    punteggioobiettivo++;
+    lcd.setCursor(7, 1);
+    lcd.print(punteggioobiettivo);
+  }
+}
+
+void sceltapunteggio()
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Scelta Punteggio");
+  lcd.setCursor(7, 1);
+  lcd.print(punteggioobiettivo);
+  bool finito = false;
+
+  while (!finito)
+  {
+    cambiaselezione();
+    if (BottonePremuto() == UP)
+    {
+      while (BottonePremuto() == UP)
+      {  }
+      controlloselezione();
+    }
+  }
+}
+
+void inizia()
+{
+  lcd.clear();
+  lcd.setCursor(2, 0);
+  lcd.print("Inizio gioco");
+  aspetta();
+}
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if (turnopartita == 0)
+  {
+    inizia();
+    sceltapunteggio();
+  }
 }
